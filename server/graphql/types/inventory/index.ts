f3c523c0489bce55856933ea7d2de65eb89019db
Hearty Oh <heartyoh@hatiolab.com>
2019-09-18 10:24:08 +0800

    Fix previledge for movement and inventory

--- server/graphql/types/inventory/index.ts
+++ server/graphql/types/inventory/index.ts
@@ -6,21 +6,21 @@ import { NewInventory } from './new-inventory'
 export const Mutation = `
   createInventory (
     inventory: NewInventory!
-  ): Inventory @priviledge(category: "warehouse", priviledge: "mutation")
+  ): Inventory @priviledge(category: "inventory", priviledge: "mutation")
 
   updateInventory (
     name: String!
     patch: InventoryPatch!
-  ): Inventory @priviledge(category: "warehouse", priviledge: "mutation")
+  ): Inventory @priviledge(category: "inventory", priviledge: "mutation")
 
   deleteInventory (
     name: String!
-  ): Boolean @priviledge(category: "warehouse", priviledge: "mutation")
+  ): Boolean @priviledge(category: "inventory", priviledge: "mutation")
     `
 
 export const Query = `
-inventories(filters: [Filter], pagination: Pagination, sortings: [Sorting]): InventoryList @priviledge(category: "warehouse", priviledge: "query")
-inventory(id: String!): Inventory @priviledge(category: "warehouse", priviledge: "query")
+inventories(filters: [Filter], pagination: Pagination, sortings: [Sorting]): InventoryList @priviledge(category: "inventory", priviledge: "query")
+inventory(id: String!): Inventory @priviledge(category: "inventory", priviledge: "query")
 `
 
 export const Types = [Inventory, NewInventory, InventoryPatch, InventoryList]
--- server/graphql/types/movement/index.ts
+++ server/graphql/types/movement/index.ts
@@ -7,21 +7,21 @@ import { directivePriviledge } from '@things-factory/auth-base'
 export const Mutation = `
   createMovement (
     movement: NewMovement!
-  ): Movement @priviledge(category: "warehouse", priviledge: "mutation")
+  ): Movement @priviledge(category: "movement", priviledge: "mutation")
 
   updateMovement (
     name: String!
     patch: MovementPatch!
-  ): Movement @priviledge(category: "warehouse", priviledge: "mutation")
+  ): Movement @priviledge(category: "movement", priviledge: "mutation")
 
   deleteMovement (
     name: String!
-  ): Boolean @priviledge(category: "warehouse", priviledge: "mutation")
+  ): Boolean @priviledge(category: "movement", priviledge: "mutation")
     `
 
 export const Query = `
-  movements(filters: [Filter], pagination: Pagination, sortings: [Sorting]): MovementList @priviledge(category: "warehouse", priviledge: "query")
-  movement(id: String!): Movement @priviledge(category: "warehouse", priviledge: "query")
+  movements(filters: [Filter], pagination: Pagination, sortings: [Sorting]): MovementList @priviledge(category: "movement", priviledge: "query")
+  movement(id: String!): Movement @priviledge(category: "movement", priviledge: "query")
 `
 
 export const Types = [Movement, NewMovement, MovementPatch, MovementList]
