8d6cf5c9dd113b57446699f3c71a9e83ab2a3ce5
leemyunggyu <jaylee@hatiolab.com>
2019-09-29 22:12:30 +0800

    v0.0.65

--- server/graphql/resolvers/inventory/index.ts
+++ server/graphql/resolvers/inventory/index.ts
@@ -1,13 +1,16 @@
-import { inventoryResolver } from './inventory'
-import { inventoriesResolver } from './inventories'
-
-import { updateInventory } from './update-inventory'
 import { createInventory } from './create-inventory'
 import { deleteInventory } from './delete-inventory'
+import { intransitInventories } from './intransit-inventories'
+import { inventoriesResolver } from './inventories'
+import { inventoryResolver } from './inventory'
+import { onhandInventories } from './onhand-inventories'
+import { updateInventory } from './update-inventory'
 
 export const Query = {
   ...inventoriesResolver,
-  ...inventoryResolver
+  ...inventoryResolver,
+  ...onhandInventories,
+  ...intransitInventories
 }
 
 export const Mutation = {
