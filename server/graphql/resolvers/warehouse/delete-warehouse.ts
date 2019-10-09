caec5caba6ed919883bb49f8207a8059b0ba1104
horwengliang <horwengliang@gmail.com>
2019-10-09 20:52:50 +0800

    v0.0.89

--- server/graphql/resolvers/warehouse/delete-warehouse.ts
+++ server/graphql/resolvers/warehouse/delete-warehouse.ts
@@ -1,8 +1,22 @@
-import { getRepository } from 'typeorm'
-import { Warehouse } from '../../../entities'
+import { getRepository, In } from 'typeorm'
+import { Warehouse, Location } from '../../../entities'
 
 export const deleteWarehouse = {
   async deleteWarehouse(_: any, { name }, context: any) {
+    let foundWarehouse: Warehouse = await getRepository(Warehouse).findOne({
+      where: { domain: context.state.domain, name },
+      relations: ['domain', 'bizplace', 'locations', 'creator', 'updater']
+    })
+    if (!foundWarehouse) throw new Error(`Warehouse doesn't exists.`)
+    const foundLocations: Location[] = foundWarehouse.locations
+
+    if (foundLocations) {
+      const locIds = foundLocations.map((loc: Location) => loc.id)
+      if (locIds.length) {
+        await getRepository(Location).delete({ id: In(locIds) })
+      }
+    }
+
     await getRepository(Warehouse).delete({ domain: context.state.domain, name })
     return true
   }
