0b3b103e7bbf8c733c78f9f1c5cde04773d6018c
leemyunggyu <jaylee@hatiolab.com>
2019-10-23 10:11:31 +0900

    v0.0.107

--- server/graphql/resolvers/inventory/inventories.ts
+++ server/graphql/resolvers/inventory/inventories.ts
@@ -1,10 +1,16 @@
+import { Bizplace } from '@things-factory/biz-base'
 import { convertListParams, ListParam } from '@things-factory/shell'
-import { getRepository } from 'typeorm'
+import { getRepository, In } from 'typeorm'
 import { Inventory } from '../../../entities'
 
 export const inventoriesResolver = {
   async inventories(_: any, params: ListParam, context: any) {
     const convertedParams = convertListParams(params)
+    convertedParams.where = {
+      ...convertedParams.where,
+      domain: context.state.domain,
+      bizplace: In(context.state.bizplaces.map((bizplace: Bizplace) => bizplace.id))
+    }
 
     const [items, total] = await getRepository(Inventory).findAndCount({
       ...convertedParams,
