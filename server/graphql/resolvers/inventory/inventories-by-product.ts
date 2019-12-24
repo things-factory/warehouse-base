41ec7c75e5e1d5689e6a7339eab202d978158753
farishelmi <farishelmi@hatiolab.com>
2019-12-24 21:36:11 +0800

    v0.9.42
    Revert the changes in inventories-by-product.ts because in this branch, there is no getPermitedBizplace.

--- server/graphql/resolvers/inventory/inventories-by-product.ts
+++ server/graphql/resolvers/inventory/inventories-by-product.ts
@@ -1,4 +1,4 @@
-import { getPermittedBizplaceIds } from '@things-factory/biz-base'
+import { Bizplace } from '@things-factory/biz-base'
 import { Product } from '@things-factory/product-base'
 import { convertListParams, ListParam } from '@things-factory/shell'
 import { getRepository, SelectQueryBuilder } from 'typeorm'
@@ -6,68 +6,51 @@ import { Inventory } from '../../../entities'
 
 export const inventoriesByProduct = {
   async inventoriesByProduct(_: any, params: ListParam, context: any) {
-    let products: Product[]
-    const permittedBizplaceIds: string[] = await getPermittedBizplaceIds(context.state.domain, context.state.user)
+    const convertedParams = convertListParams({
+      filters: [
+        ...params.filters,
+        {
+          name: 'bizplace',
+          operator: 'in',
+          value: context.state.bizplaces.map((bizplace: Bizplace) => bizplace.id)
+        }
+      ]
+    })
 
-    if (params?.filters?.length) {
-      const convertedParams = convertListParams({
-        filters: [
-          ...params.filters,
-          {
-            name: 'bizplace',
-            operator: 'in',
-            value: permittedBizplaceIds
-          }
-        ]
-      })
-
-      products = await getRepository(Product).find(convertedParams)
-    }
+    const products = await getRepository(Product).find({
+      ...convertedParams
+    })
 
     const page = params.pagination.page
     const limit = params.pagination.limit
 
-    const selectQueryBuilder: SelectQueryBuilder<Inventory> = getRepository(Inventory).createQueryBuilder()
-    const countQueryBuilder: SelectQueryBuilder<Inventory> = getRepository(Inventory).createQueryBuilder()
-    selectQueryBuilder
+    const queryBuilder = getRepository(Product).createQueryBuilder()
+    queryBuilder
       .select('Product.id', 'id')
       .addSelect('Product.name', 'name')
       .addSelect('Product.description', 'description')
       .addSelect('Product.weight', 'weight')
+      .addSelect('Product.unit', 'unit')
       .addSelect('Product.type', 'type')
       .addSelect('ProductRef.name', 'productRefName')
       .addSelect('ProductRef.description', 'productRefDesciption')
       .addSelect('SUM(Inventory.qty)', 'qty')
-      .leftJoin('Inventory.product', 'Product')
       .leftJoin('Product.productRef', 'ProductRef')
-      .where('Inventory.qty >= :qty', { qty: 0 })
-      .andWhere('Inventory.domain_id = :domainId', { domainId: context.state.domain.id })
-      .andWhere('Inventory.bizplace_id IN (:...bizplaceIds)', { bizplaceIds: permittedBizplaceIds })
+      .leftJoin(Inventory, 'Inventory', 'Inventory.product_id = Product.id')
+      .where('qty >= :qty', { qty: 0 })
       .offset((page - 1) * limit)
       .limit(limit)
       .groupBy('Product.id')
       .addGroupBy('ProductRef.id')
 
-    countQueryBuilder
-      .select('COUNT(DISTINCT("Inventory"."product_id"))', 'total')
-      .leftJoin('Inventory.product', 'Product')
-      .leftJoin('Product.productRef', 'ProductRef')
-      .where('Inventory.qty >= :qty', { qty: 0 })
-      .andWhere('Inventory.domain_id = :domainId', { domainId: context.state.domain.id })
-      .andWhere('Inventory.bizplace_id IN (:...bizplaceIds)', { bizplaceIds: permittedBizplaceIds })
-
-    if (products) {
-      let productIds: string[] = products.map((product: Product) => product.id)
-      if (productIds.length === 0) {
-        productIds = [null]
-      }
-
-      selectQueryBuilder.andWhere('Product.id IN (:...productIds)', { productIds })
-      countQueryBuilder.andWhere('Product.id IN (:...productIds)', { productIds })
+    if (products && products.length) {
+      queryBuilder.andWhereInIds(products.map((product: Product) => product.id))
+    } else {
+      queryBuilder.andWhereInIds([null])
     }
 
-    const items = await selectQueryBuilder.getRawMany()
-    const { total } = await countQueryBuilder.getRawOne()
+    const items = await queryBuilder.getRawMany()
+    const total = await queryBuilder.getCount()
 
     return {
       items: items.map((item: any) => {
@@ -78,12 +61,13 @@ export const inventoriesByProduct = {
             description: item.description,
             type: item.type,
             weight: item.weight,
+            unit: item.unit,
             productRef: { name: item.productRefName, description: item.productRefDesciption }
           },
           qty: item.qty
         }
       }),
-      total: Number(total)
+      total
     }
   }
 }
