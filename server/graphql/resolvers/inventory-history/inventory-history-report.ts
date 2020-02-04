03b8182c28b3920d4bcd02f883206a4a30f083dc
ChrisLim <chrislim@hatiolab.com>
2020-02-04 20:49:45 +0800

    v2.0.14

--- server/graphql/resolvers/inventory-history/inventory-history-report.ts
+++ server/graphql/resolvers/inventory-history/inventory-history-report.ts
@@ -58,12 +58,28 @@ export const inventoryHistoryReport = {
       }
 
       const result = await getRepository(InventoryHistory).query(`
-        ;WITH invhCte AS
+        ;WITH dataSrc AS
         (
-          SELECT prd.name AS product_name, prd.description AS product_description, invh.batch_id, invh.product_id, invh.packing_type, invh.bizplace_id, invh.domain_id,
-          SUM(COALESCE(invh.opening_qty,0) + COALESCE(invh.qty,0)) AS qty,
+          SELECT prd.name AS product_name, prd.description AS product_description, invh.batch_id, invh.product_id, 
+          invh.packing_type, invh.bizplace_id, invh.domain_id,
+          invh.ref_order_id, invh.order_no, invh.order_ref_no, invh.transaction_type, invh.created_at,
+          invh.qty, invh.opening_qty, invh.weight, invh.opening_weight
+          FROM inventory_histories invh
+          INNER JOIN products prd ON cast(prd.id AS VARCHAR) = invh.product_id
+          WHERE
+          invh.transaction_type IN ('NEW', 'ADJUSTMENT', 'UNLOADING', 'PICKING', 'LOADING', 'UNDO_UNLOADING')
+          AND invh.domain_id = '${context.state.domain.id}'
+          AND invh.bizplace_id = '${bizplace.id}'
+          AND invh.created_at BETWEEN '${new Date(fromDate.value).toLocaleDateString()} 00:00:00'
+          AND '${new Date(toDate.value).toLocaleDateString()} 23:59:59'
+          ${productQuery}
+        )
+        SELECT * FROM (
+          SELECT src.product_name, src.product_description, src.batch_id, src.product_id, src.packing_type, 
+          src.bizplace_id, src.domain_id,
+          SUM(COALESCE(invh.qty,0)) AS qty,
           0 AS opening_qty,
-          SUM(COALESCE(invh.opening_weight,0) + COALESCE(invh.weight,0)) AS weight,
+          SUM(COALESCE(invh.weight,0)) AS weight,
           0 AS opening_weight,
           'Opening Balance' AS order_name,
           '-' AS ref_no,
@@ -71,43 +87,40 @@ export const inventoryHistoryReport = {
           to_timestamp('${new Date(
             fromDate.value
           ).toLocaleDateString()} 00:00:00', 'MM/DD/YYYY HH24:MI:SS') AS created_at
-          FROM inventory_histories invh
-          INNER JOIN products prd on cast(prd.id AS VARCHAR) = invh.product_id
-          WHERE    
-          invh.transaction_type in ('NEW', 'ADJUSTMENT', 'UNLOADING', 'PICKNG', 'LOADING', 'UNDO_UNLOADING')
-          AND invh.domain_id = '${context.state.domain.id}'
-          AND invh.bizplace_id = '${bizplace.id}'
-          AND invh.created_at <'${new Date(fromDate.value).toLocaleDateString()} 00:00:00'
-          ${productQuery}
-          GROUP BY prd.name, prd.description, invh.batch_id, invh.product_id, invh.packing_type, invh.bizplace_id, invh.domain_id
-        )
-        SELECT * FROM (
-          SELECT *
-          FROM invhCte
-          UNION ALL
-          SELECT prd.name AS product_name, prd.description AS product_description, invh.batch_id, invh.product_id, invh.packing_type, invh.bizplace_id, invh.domain_id,
-          invh.qty, invh.opening_qty,
-          invh.weight, invh.opening_weight,
-          CASE WHEN invh.transaction_type = 'ADJUSTMENT' THEN 'ADJUSTMENT'
-               WHEN invh.transaction_type = 'NEW' THEN 'NEW'
-               ELSE COALESCE(order_no, '-') END AS order_name,
-          CASE WHEN invh.transaction_type = 'ADJUSTMENT' THEN 'ADJUSTMENT' 
-               WHEN invh.transaction_type = 'NEW' THEN 'NEW'
-          	   ELSE COALESCE(order_ref_no, '-') END AS ref_no,
-          1 AS rn, invh.created_at
-          FROM inventory_histories invh
-          LEFT JOIN arrival_notices arrNo ON cast(arrNo.id AS VARCHAR) = invh.ref_order_id AND (invh.transaction_type = 'UNLOADING' OR invh.transaction_type = 'UNDO_UNLOADING')
-          LEFT JOIN worksheets wks ON cast(wks.id AS VARCHAR) = invh.ref_order_id AND invh.transaction_type = 'PICKING'
-          LEFT JOIN release_goods rel ON cast(rel.id AS VARCHAR) = cast(wks.release_good_id AS VARCHAR) AND invh.transaction_type = 'PICKING'
-          INNER JOIN products prd on cast(prd.id AS VARCHAR) = invh.product_id
-          WHERE
-          invh.transaction_type IN ('NEW', 'ADJUSTMENT', 'UNLOADING', 'PICKNG', 'LOADING', 'UNDO_UNLOADING')
-          AND invh.domain_id = '${context.state.domain.id}'
-          AND invh.bizplace_id = '${bizplace.id}'
-          AND invh.created_at BETWEEN '${new Date(fromDate.value).toLocaleDateString()} 00:00:00'
-          AND '${new Date(toDate.value).toLocaleDateString()} 23:59:59'
-          ${productQuery}
-        ) AS src order by product_name asc, packing_type asc, batch_id asc, rn asc, created_at asc
+          FROM (
+          SELECT src.product_name, src.product_description, src.batch_id, src.product_id, src.packing_type, src.bizplace_id, 
+          src.domain_id
+          FROM dataSrc src
+          GROUP BY src.product_name, src.product_description, src.batch_id, src.product_id, src.packing_type, src.bizplace_id, 
+          src.domain_id
+        ) AS src 
+        LEFT JOIN inventory_histories invh ON src.batch_id = invh.batch_id AND 
+        src.product_id = invh.product_id AND 
+        src.packing_type = invh.packing_type AND 
+        src.bizplace_id = invh.bizplace_id AND 
+        src.domain_id = invh.domain_id AND
+        invh.created_at < '${new Date(fromDate.value).toLocaleDateString()} 00:00:00' AND
+        invh.transaction_type IN ('NEW', 'ADJUSTMENT', 'UNLOADING', 'PICKING', 'LOADING', 'UNDO_UNLOADING')
+        GROUP BY src.product_name, src.product_description, src.batch_id, src.product_id, src.packing_type, src.bizplace_id, 
+        src.domain_id
+        UNION ALL
+        SELECT invh.product_name, invh.product_description, invh.batch_id, invh.product_id, invh.packing_type, invh.bizplace_id, 
+        invh.domain_id,
+        invh.qty, invh.opening_qty,	invh.weight, invh.opening_weight,
+        CASE WHEN invh.transaction_type = 'ADJUSTMENT' THEN 'ADJUSTMENT'
+          WHEN invh.transaction_type = 'NEW' THEN 'NEW'
+          ELSE COALESCE(order_no, '-') END AS order_name,
+        CASE WHEN invh.transaction_type = 'ADJUSTMENT' THEN 'ADJUSTMENT' 
+          WHEN invh.transaction_type = 'NEW' THEN 'NEW'
+          ELSE COALESCE(order_ref_no, '-') END AS ref_no,
+        1 AS rn, invh.created_at
+        FROM dataSrc invh
+        LEFT JOIN arrival_notices arrNo ON cast(arrNo.id AS VARCHAR) = invh.ref_order_id AND (invh.transaction_type = 'UNLOADING' OR 
+          invh.transaction_type = 'UNDO_UNLOADING')
+        LEFT JOIN worksheets wks ON cast(wks.id AS VARCHAR) = invh.ref_order_id AND invh.transaction_type = 'PICKING'
+        LEFT JOIN release_goods rel ON cast(rel.id AS VARCHAR) = cast(wks.release_good_id AS VARCHAR) AND 
+          invh.transaction_type = 'PICKING'
+        ) AS reportData ORDER BY product_name asc, packing_type asc, batch_id asc, rn asc, created_at asc
       `)
 
       let items = result as any
