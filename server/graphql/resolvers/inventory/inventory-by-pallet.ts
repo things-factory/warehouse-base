454ef7a4c6616eb8ae08dce6a7ec54de24a16bde
leemyunggyu <jaylee@hatiolab.com>
2020-05-19 12:30:34 +0900

    status condition for inventory by pallet

--- server/graphql/resolvers/inventory/inventory-by-pallet.ts
+++ server/graphql/resolvers/inventory/inventory-by-pallet.ts
@@ -1,10 +1,11 @@
-import { getRepository } from 'typeorm'
+import { Equal, getRepository, Not } from 'typeorm'
+import { INVENTORY_STATUS } from '../../../constants'
 import { Inventory } from '../../../entities'
 
 export const inventoryByPalletResolver = {
   async inventoryByPallet(_: any, { palletId }, context: any) {
     return await getRepository(Inventory).findOne({
-      where: { domain: context.state.domain, palletId },
+      where: { domain: context.state.domain, palletId, status: Not(Equal(INVENTORY_STATUS.TERMINATED)) },
       relations: ['domain', 'bizplace', 'product', 'location', 'warehouse', 'creator', 'updater']
     })
   }
