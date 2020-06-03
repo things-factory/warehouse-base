078228645e323da3b9e0b19c4503f708bb5f1257
horwengliang <horwengliang@gmail.com>
2020-06-03 12:24:40 +0800

    order desc for qty

--- server/graphql/resolvers/inventory/inventories-by-product.ts
+++ server/graphql/resolvers/inventory/inventories-by-product.ts
@@ -42,7 +42,7 @@ export const inventoriesByProduct = {
       .leftJoin('Inventory.product', 'Product')
       .leftJoin('Product.productRef', 'ProductRef')
       .innerJoin('Product.bizplace', 'Bizplace')
-      .where('Inventory.qty > :qty', { qty: 0 })
+      .where('Inventory.qty >= :qty', { qty: 0 })
       .andWhere('Inventory.domain_id = :domainId', { domainId: context.state.domain.id })
       .andWhere('Inventory.bizplace_id IN (:...bizplaceIds)', { bizplaceIds: permittedBizplaceIds })
       .offset((page - 1) * limit)
@@ -50,6 +50,7 @@ export const inventoriesByProduct = {
       .groupBy('Product.id')
       .addGroupBy('ProductRef.id')
       .addGroupBy('Bizplace.id')
+      .orderBy('SUM(Inventory.qty)', 'DESC')
 
     countQueryBuilder
       .select('COUNT(DISTINCT("Inventory"."product_id"))', 'total')
