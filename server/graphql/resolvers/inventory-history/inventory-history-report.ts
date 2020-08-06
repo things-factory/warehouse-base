b64063e71c4b9387f1988e6dc2b354f5249da267
ChrisLim <chrislim@hatiolab.com>
2020-08-06 16:59:57 +0800

    Update inventory-history-report.ts
    - Added new Filter Option
    - Update Query method
    - Group pallet of same transaction, product, batch, packing type, and date instead of each pallet per row.

--- server/graphql/resolvers/inventory-history/inventory-history-report.ts
+++ server/graphql/resolvers/inventory-history/inventory-history-report.ts
@@ -1,5 +1,5 @@
 import { convertListParams, ListParam } from '@things-factory/shell'
-import { getRepository } from 'typeorm'
+import { getManager, EntityManager, getRepository } from 'typeorm'
 import { User } from '@things-factory/auth-base'
 import { Bizplace, BizplaceUser } from '@things-factory/biz-base'
 import { InventoryHistory } from '../../../entities'
@@ -10,6 +10,8 @@ export const inventoryHistoryReport = {
       let userFilter = params.filters.find(data => data.name === 'user')
 
       let bizplaceFilter = { name: '', operator: '', value: '' }
+
+      let hasTransactionOrBalanceFilter = params.filters.find(data => data.name === 'hasTransactionOrBalance')
 
       if (userFilter) {
         const user: User = await getRepository(User).findOne({
@@ -35,6 +37,7 @@ export const inventoryHistoryReport = {
 
       let fromDate = params.filters.find(data => data.name === 'fromDate')
       let toDate = params.filters.find(data => data.name === 'toDate')
+      let batchNo = params.filters.find(data => data.name === 'batch_no')
       let product = params.filters.find(data => data.name === 'product')
 
       if (!bizplaceFilter || !fromDate || !toDate) throw 'Invalid input'
@@ -42,6 +45,19 @@ export const inventoryHistoryReport = {
       const bizplace: Bizplace = await getRepository(Bizplace).findOne({
         id: bizplaceFilter.value
       })
+
+      let batchNoQuery = ''
+      if (batchNo) {
+        batchNoQuery =
+          'AND invh.batch_id ILIKE ANY(ARRAY[' +
+          batchNo.value
+            .split(',')
+            .map(prod => {
+              return "'%" + prod.trim().replace(/'/g, "''") + "%'"
+            })
+            .join(',') +
+          '])'
+      }
 
       let productQuery = ''
       if (product) {
@@ -56,90 +72,135 @@ export const inventoryHistoryReport = {
           '])'
       }
 
-      const result = await getRepository(InventoryHistory).query(`
-        ;WITH dataSrc AS
+      let hasTransactionOrBalanceQuery = ''
+      if (hasTransactionOrBalanceFilter && hasTransactionOrBalanceFilter.value) {
+        hasTransactionOrBalanceQuery = 'and (src.totalRow > 1 or src.totalQty <> 0)'
+      }
+
+      return await getManager().transaction(async (trxMgr: EntityManager) => {
+        await trxMgr.query(
+          `
+        create temp table temp_data_src AS
         (
-          SELECT prd.name AS product_name, prd.description AS product_description, invh.batch_id, invh.product_id, 
+          SELECT prd.name AS product_name, prd.description AS product_description, trim(invh.batch_id) as batch_id, invh.product_id,
           invh.packing_type, invh.bizplace_id, invh.domain_id,
           invh.ref_order_id, invh.order_no, invh.order_ref_no, invh.transaction_type, invh.created_at,
-          invh.qty, invh.opening_qty, invh.weight, invh.opening_weight
+          invh.qty, invh.opening_qty, COALESCE(invh.weight, 0) as weight, COALESCE(invh.opening_weight, 0) as opening_weight
           FROM inventory_histories invh
           INNER JOIN products prd ON cast(prd.id AS VARCHAR) = invh.product_id
           WHERE
           invh.transaction_type IN ('NEW', 'ADJUSTMENT', 'UNLOADING', 'PICKING', 'LOADING', 'UNDO_UNLOADING', 'CANCEL_ORDER', 'RETURN')
-          AND invh.domain_id = '${context.state.domain.id}'
-          AND invh.bizplace_id = '${bizplace.id}'
-          AND invh.created_at < '${new Date(toDate.value).toLocaleDateString()} 23:59:59'
+          AND invh.domain_id = $1
+          AND invh.bizplace_id = $2
+          AND invh.created_at::date <= $3
+          ${batchNoQuery}
           ${productQuery}
+        ) 
+      `,
+          [context.state.domain.id, bizplace.id, new Date(toDate.value).toISOString()]
         )
-        SELECT * FROM (
-          SELECT src.product_name, src.product_description, src.batch_id, src.product_id, src.packing_type, 
-          src.bizplace_id, src.domain_id,
-          SUM(COALESCE(invh.qty,0)) AS qty,
-          0 AS opening_qty,
-          SUM(COALESCE(invh.weight,0)) AS weight,
-          0 AS opening_weight,
-          'Opening Balance' AS order_name,
-          '-' AS ref_no,
-          0 AS rn,
-          to_timestamp('${new Date(
-            fromDate.value
-          ).toLocaleDateString()} 00:00:00', 'MM/DD/YYYY HH24:MI:SS') AS created_at
-          FROM (
-            SELECT src.product_name, src.product_description, src.batch_id, src.product_id, src.packing_type, src.bizplace_id, 
-            src.domain_id
-            FROM dataSrc src
+
+        await trxMgr.query(
+          `
+          create temp table temp_inv_history as (
+            SELECT src.product_name, src.product_description, src.batch_id, src.product_id, src.packing_type, 
+            src.bizplace_id, src.domain_id,
+            SUM(COALESCE(invh.qty,0)) AS qty,
+            0 AS opening_qty,
+            SUM(COALESCE(invh.weight,0)) AS weight,
+            0 AS opening_weight,
+            'Opening Balance' AS order_name,
+            '-' AS ref_no,
+            0 AS rn,
+            $1 AS created_at
+            FROM (
+              SELECT src.product_name, src.product_description, src.batch_id, src.product_id, src.packing_type, src.bizplace_id, 
+              src.domain_id
+              FROM temp_data_src src
+              GROUP BY src.product_name, src.product_description, src.batch_id, src.product_id, src.packing_type, src.bizplace_id, 
+              src.domain_id
+            ) AS src 
+            LEFT JOIN inventory_histories invh ON src.batch_id = trim(invh.batch_id) AND 
+            src.product_id = invh.product_id AND 
+            src.packing_type = invh.packing_type AND 
+            src.bizplace_id = invh.bizplace_id AND 
+            src.domain_id = invh.domain_id AND
+            invh.created_at < $1::timestamp AND
+            invh.transaction_type IN ('NEW', 'ADJUSTMENT', 'UNLOADING', 'PICKING', 'LOADING', 'UNDO_UNLOADING', 'CANCEL_ORDER', 'RETURN')
             GROUP BY src.product_name, src.product_description, src.batch_id, src.product_id, src.packing_type, src.bizplace_id, 
             src.domain_id
-          ) AS src 
-          LEFT JOIN inventory_histories invh ON src.batch_id = invh.batch_id AND 
-          src.product_id = invh.product_id AND 
-          src.packing_type = invh.packing_type AND 
-          src.bizplace_id = invh.bizplace_id AND 
-          src.domain_id = invh.domain_id AND
-          invh.created_at < '${new Date(fromDate.value).toLocaleDateString()} 00:00:00' AND
-          invh.transaction_type IN ('NEW', 'ADJUSTMENT', 'UNLOADING', 'PICKING', 'LOADING', 'UNDO_UNLOADING', 'CANCEL_ORDER', 'RETURN')
-          GROUP BY src.product_name, src.product_description, src.batch_id, src.product_id, src.packing_type, src.bizplace_id, 
-          src.domain_id
-          UNION ALL
-          SELECT invh.product_name, invh.product_description, invh.batch_id, invh.product_id, invh.packing_type, invh.bizplace_id, 
-          invh.domain_id,
-          invh.qty, invh.opening_qty,	invh.weight, invh.opening_weight,
-          CASE WHEN invh.transaction_type = 'ADJUSTMENT' THEN 'ADJUSTMENT'
-            WHEN invh.transaction_type = 'NEW' THEN 'NEW'
-            ELSE COALESCE(order_no, '-') END AS order_name,
-          CASE WHEN invh.transaction_type = 'ADJUSTMENT' THEN 'ADJUSTMENT' 
-            WHEN invh.transaction_type = 'NEW' THEN 'NEW'
-            ELSE COALESCE(order_ref_no, '-') END AS ref_no,
-          1 AS rn, invh.created_at
-          FROM dataSrc invh
-          LEFT JOIN arrival_notices arrNo ON cast(arrNo.id AS VARCHAR) = invh.ref_order_id AND (invh.transaction_type = 'UNLOADING' OR 
-            invh.transaction_type = 'UNDO_UNLOADING')
-          LEFT JOIN worksheets wks ON cast(wks.id AS VARCHAR) = invh.ref_order_id AND invh.transaction_type = 'PICKING'
-          LEFT JOIN release_goods rel ON cast(rel.id AS VARCHAR) = cast(wks.release_good_id AS VARCHAR) AND 
-            invh.transaction_type = 'PICKING'
-          WHERE (invh.qty <> 0 OR invh.weight <> 0)
-          AND invh.created_at BETWEEN '${new Date(fromDate.value).toLocaleDateString()} 00:00:00'
-            AND '${new Date(toDate.value).toLocaleDateString()} 23:59:59'
-        ) AS reportData ORDER BY product_name asc, product_description asc, packing_type asc, batch_id asc, rn asc, created_at asc
-      `)
+            UNION ALL
+            SELECT product_name, product_description, batch_id, product_id, packing_type, bizplace_id, 
+            domain_id, sum(qty) as qty, sum(opening_qty) as qty, sum(weight) as weight, sum(opening_weight) as opening_weight, order_name, ref_no, rn, MIN(created_at) as created_at
+            FROM (
+              SELECT invh.product_name, invh.product_description, invh.batch_id, invh.product_id, invh.packing_type, invh.bizplace_id, 
+              invh.domain_id,
+              invh.qty, invh.opening_qty,	invh.weight, invh.opening_weight,
+              CASE WHEN invh.transaction_type = 'ADJUSTMENT' THEN 'ADJUSTMENT'
+                WHEN invh.transaction_type = 'NEW' THEN 'NEW'
+                ELSE COALESCE(order_no, '-') END AS order_name,
+              CASE WHEN invh.transaction_type = 'ADJUSTMENT' THEN 'ADJUSTMENT' 
+                WHEN invh.transaction_type = 'NEW' THEN 'NEW'
+                ELSE COALESCE(order_ref_no, '-') END AS ref_no,
+              1 AS rn, invh.created_at
+              FROM temp_data_src invh
+              LEFT JOIN arrival_notices arrNo ON cast(arrNo.id AS VARCHAR) = invh.ref_order_id AND (invh.transaction_type = 'UNLOADING' OR 
+                invh.transaction_type = 'UNDO_UNLOADING')
+              LEFT JOIN worksheets wks ON cast(wks.id AS VARCHAR) = invh.ref_order_id AND invh.transaction_type = 'PICKING'
+              LEFT JOIN release_goods rel ON cast(rel.id AS VARCHAR) = cast(wks.release_good_id AS VARCHAR) AND 
+                invh.transaction_type = 'PICKING'
+              WHERE (invh.qty <> 0 OR invh.weight <> 0)
+              AND invh.created_at BETWEEN $1::timestamp AND $2::timestamp
+            ) AS inv_movement 
+            GROUP BY product_name, product_description, batch_id, product_id, packing_type, bizplace_id, 
+            domain_id, order_name, ref_no, rn
+          )`,
+          [new Date(fromDate.value).toISOString(), new Date(toDate.value).toISOString()]
+        )
 
-      let items = result as any
-      items = items.map(item => {
-        return {
-          batchId: item.batch_id,
-          bizplace: bizplace,
-          packingType: item.packing_type,
-          product: { name: item.product_name + ' ( ' + item.product_description + ' )' },
-          qty: item.qty,
-          weight: item.weight,
-          orderName: item.order_name,
-          orderRefNo: item.ref_no,
-          createdAt: item.created_at
-        }
+        const result: any = await trxMgr.query(
+          ` 
+          select product_name, product_description, batch_id, product_id, packing_type, bizplace_id, 
+          domain_id, qty, opening_qty, weight, opening_weight, order_name, ref_no, created_at::date
+          from temp_inv_history invh where
+          exists (
+            select * from (
+              select batch_id, product_name, packing_type, sum(qty) as totalQty, count(*) as totalRow from temp_inv_history ih2 group by batch_id, product_name, packing_type
+              ) src where src.batch_id = invh.batch_id and src.product_name = invh.product_name and src.packing_type = invh.packing_type
+              ${hasTransactionOrBalanceQuery}
+          )
+          ORDER BY invh.product_name asc, invh.product_description asc, invh.packing_type asc, invh.batch_id asc, invh.rn asc, invh.created_at asc
+        `
+        )
+
+        trxMgr.query(
+          `
+          drop table temp_data_src, temp_inv_history
+        `
+        )
+
+        let items = result as any
+        items = items.map(item => {
+          return {
+            batchId: item.batch_id,
+            bizplace: bizplace,
+            packingType: item.packing_type,
+            product: {
+              id: item.product_id,
+              name: item.product_name + ' ( ' + item.product_description + ' )'
+            },
+            qty: item.qty,
+            weight: item.weight,
+            openingQty: item.opening_qty,
+            openingWeight: item.opening_weight,
+            orderName: item.order_name,
+            orderRefNo: item.ref_no,
+            createdAt: item.created_at
+          }
+        })
+
+        return items
       })
-
-      return items
     } catch (error) {
       throw error
     }
