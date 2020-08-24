362e5ddb08aa5da00d4e500e22d94a5a04bb14d6
ChrisLim <chrislim@hatiolab.com>
2020-08-24 13:05:56 +0800

    v2.7.2
    Specified Query Columns

--- server/entities/reduced-inventory-history.ts
+++ server/entities/reduced-inventory-history.ts
@@ -4,16 +4,16 @@ import { Domain } from '@things-factory/shell'
 
 @ViewEntity({
   expression: `
-    select * from inventory_histories ih 
+    select ih.* from inventory_histories ih 
     where not exists (
-      select ih2.* from inventory_histories ih2 
+      select ih2.id from inventory_histories ih2 
       where (ih2.transaction_type ='RETURN' or ih2.transaction_type ='CANCEL_ORDER') and
       ih2.domain_id = ih.domain_id and
       ih2.pallet_id = ih.pallet_id and
       ih2.ref_order_id = ih.ref_order_id
     ) and not exists 
     (
-      select * from inventory_histories ih3 where
+      select ih3.id from inventory_histories ih3 where
       ih3.domain_id = ih.domain_id and 
       ih3.pallet_id = ih.pallet_id and 
       (ih3.seq = ih.seq + 1 or ih3.seq = ih.seq) and 
