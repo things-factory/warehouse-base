340720c19b5a9396dfbda6b5f1f5645b068dc6b8
ChrisLim <chrislim@hatiolab.com>
2020-11-30 14:34:49 +0800

    v2.7.0-uat.21
    Fix unable to search customer

--- server/graphql/resolvers/inventory-change/inventory-changes.ts
+++ server/graphql/resolvers/inventory-change/inventory-changes.ts
@@ -1,40 +1,49 @@
-import { getPermittedBizplaceIds } from '@things-factory/biz-base'
+import { getPermittedBizplaceIds, Bizplace } from '@things-factory/biz-base'
 import { Product } from '@things-factory/product-base'
-import { convertListParams, ListParam } from '@things-factory/shell'
-import { getRepository } from 'typeorm'
+import { convertListParams, ListParam, buildQuery } from '@things-factory/shell'
+import { getRepository, SelectQueryBuilder, Brackets } from 'typeorm'
 import { InventoryChange, Location, Warehouse } from '../../../entities'
 
 export const inventoryChangesResolver = {
   async inventoryChanges(_: any, params: ListParam, context: any) {
-    if (!params.filters.find((filter: any) => filter.name === 'inventory.bizplace')) {
-      params.filters.push({
-        name: 'inventory.bizplace',
-        operator: 'in',
-        value: await getPermittedBizplaceIds(context.state.domain, context.state.user)
-      })
+    let bizplaces:Bizplace[]
+    if (!params.filters.find(filter=>filter.name ==='bizplace')) {
+      bizplaces = await getPermittedBizplaceIds(context.state.domain, context.state.user)
+    }else{
+      bizplaces = [params.filters.find(filter=>filter.name ==='bizplace').value]
+      params.filters = [...params.filters.filter(filter=>filter.name !='bizplace')]
     }
 
-    const convertedParams = convertListParams(params, context.state.domain.id)
-    let [items, total] = await getRepository(InventoryChange).findAndCount({
-      ...convertedParams,
-      relations: [
-        'bizplace',
-        'inventory',
-        'inventory.bizplace',
-        'inventory.product',
-        'inventory.location',
-        'product',
-        'location',
-        'domain',
-        'lastInventoryHistory',
-        'lastInventoryHistory.bizplace',
-        'creator',
-        'updater'
-      ],
-      order: {
-        createdAt: 'DESC'
-      }
+    const qb: SelectQueryBuilder<InventoryChange> = getRepository(InventoryChange).createQueryBuilder('ic')
+    buildQuery(qb, params, context)
+
+    qb.innerJoinAndSelect('ic.domain', 'dm')
+      .leftJoinAndSelect('ic.bizplace', 'biz')
+      .leftJoinAndSelect('ic.inventory', 'iv')
+      .leftJoinAndSelect('iv.bizplace', 'ivBiz')
+      .leftJoinAndSelect('iv.product', 'ivPrd')
+      .leftJoinAndSelect('iv.location', 'ivLoc')
+      .leftJoinAndSelect('ic.product', 'product')
+      .leftJoinAndSelect('ic.location', 'location')
+      .leftJoinAndSelect('ic.lastInventoryHistory', 'livh')
+      .leftJoinAndSelect('livh.bizplace', 'livhBiz')
+      .innerJoinAndSelect('ic.creator', 'creator')
+      .innerJoinAndSelect('ic.updater', 'updater')
+      .orderBy('ic.createdAt', "DESC" )
+    
+    qb.andWhere(
+        new Brackets(qb => {
+          qb.where('"iv"."bizplace_id" IN (:...bizplaces)',)
+          qb.orWhere('"ic"."bizplace_id" IN (:...bizplaces)')
+        })
+      )
+
+    qb
+    .setParameters({
+      bizplaces: bizplaces
     })
+
+    let [items, total] = await qb.getManyAndCount()
 
     items = await Promise.all(
       items.map(async item => {
